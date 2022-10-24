be35d886de6df4074bc01e7ae2892d4120f13bc9
fancn21th <fancn21th@gmail.com>
2022-10-24 15:15:27 +0800

    layout, slot

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,30 @@
 <script>
 import HeartFast from "./components/HeartFast.vue";
 import HeartSlow from "./components/HeartSlow.vue";
+import Frame from "./components/Frame.vue";
+
 export default {
   components: {
     HeartFast,
     HeartSlow,
+    Frame,
+  },
+  data() {
+    return {
+      frames: Array.from({ length: 10 }, () => {
+        return Math.random() >= 0.5 ? "fast" : "slow";
+      }),
+    };
   },
 };
 </script>
 
 <template>
-  <div>
-    <HeartFast />
-    <HeartSlow />
+  <div style="display: flex; flex-wrap: wrap">
+    <Frame :key="index" v-for="(frame, index) in frames">
+      <HeartFast v-if="frame === 'fast'" />
+      <HeartSlow v-if="frame === 'slow'" />
+    </Frame>
   </div>
 </template>
 
---
+++ src/components/Frame.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="frame">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.frame {
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+}
+</style>
